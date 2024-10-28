08dcf391e5ed108523e4763022002de0ff458ade
Harshitha Pondari <harshithap902@gmail.com>
2024-10-28 23:36:40 +0530

    mistake in models.py  change charField to CharField in User class ,username

--- Django Project/netlfix_clone/aha/models.py
+++ Django Project/netlfix_clone/aha/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class User(models.Model):
-    username=models.charField(max_length=100)
+    username=models.CharField(max_length=100)
     email=models.EmailField()
     
     def __str__(self):
