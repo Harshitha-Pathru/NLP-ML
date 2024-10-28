27b9d6641d9a5f3fe2e45d723ff30c53ec056186
Harshitha Pondari <harshithap902@gmail.com>
2024-10-28 23:25:22 +0530

    create user class in models.py file

--- Django Project/netlfix_clone/aha/models.py
+++ Django Project/netlfix_clone/aha/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    username=models.charField(max_length=100)
+    email=models.EmailField()
+    
+    def __str__(self):
+        return self.username
+
