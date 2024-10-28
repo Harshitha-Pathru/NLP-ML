87648b6dbe80cd35f841cd691ccf0e32e6cfd70f
Harshitha Pondari <harshithap902@gmail.com>
2024-10-28 23:29:13 +0530

    create movie class in models.py

--- Django Project/netlfix_clone/aha/models.py
+++ Django Project/netlfix_clone/aha/models.py
@@ -7,3 +7,15 @@ class User(models.Model):
     def __str__(self):
         return self.username
 
+class Movie(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    release_date = models.DateField()
+    genre =  models.CharField(max_length=100)
+    url = models.URLField()
+
+    def __str__(self):
+        return self.title
+
+
+
